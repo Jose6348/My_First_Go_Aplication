48b94a34f3f367ae5c6db7b870c140e411d61851
Jose6348 <jfalasco88@gmail.com>
2025-02-12 00:05:47 -0300

    Funções

--- main.go
+++ main.go
@@ -1,14 +1,56 @@
 package main
 
-import (
-	"fmt"
-	"myFirstGoProject/pacote"
-)
+import "fmt"
+
+// "myFirstGoProject/pacote"
 
 func main() {
-	fmt.Println("Hello world")
-	fmt.Println(pacote.Bar)
+	// fmt.Println("Hello world")
+
+	// fmt.Println(pacote.Bar)
+
+	// fmt.Println(somar(1, 2))
+	// a, b := swap(10, 20)
+	// fmt.Println(a, b)
+
+	// res, rem := dividir(10, 3)
+	// fmt.Println(res, rem)
+
+	// x := somar(2)(1)
+	// fmt.Println(x)
+
+	fmt.Println(somar(10, 10, 10))
 }
+
+func somar(nums ...int) int {
+	var out int
+	for _, n := range nums {
+		out += n
+	}
+	return out
+}
+
+// func somar(a int) func(int) int {
+// 	return func(b int) int {
+// 		return a + b
+// 	}
+// }
+
+// chmando funções
+
+// func somar(a, b int) int {
+// 	return a + b
+// }
+
+// func swap(a, b int) (int, int) {
+// 	return b, a
+// }
+
+// func dividir(a, b int) (res int, rem int) {
+// 	res = a / b
+// 	rem = a & b
+// 	return res, rem
+// }
 
 //Nomes Publicos ou Privados
 
