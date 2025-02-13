9b6842627f5b0d63eb4c7e55c11f28f6c0628765
Jose6348 <jfalasco88@gmail.com>
2025-02-13 00:20:46 -0300

    Variáveis

--- main.go
+++ main.go
@@ -4,63 +4,12 @@ import "fmt"
 
 // "myFirstGoProject/pacote"
 
+var idade = 30
+
 func main() {
-	// fmt.Println("Hello world")
-
-	// fmt.Println(pacote.Bar)
-
-	// fmt.Println(somar(1, 2))
-	// a, b := swap(10, 20)
-	// fmt.Println(a, b)
-
-	// res, rem := dividir(10, 3)
-	// fmt.Println(res, rem)
-
-	// x := somar(2)(1)
-	// fmt.Println(x)
-
-	fmt.Println(somar(10, 10, 10))
+	foo, bar := "foo", 50
+	nome := "Pedro"
+	sobrenome := "Pessoa"
+	fmt.Println(nome, sobrenome, idade)
+	fmt.Println(foo, bar)
 }
-
-func somar(nums ...int) int {
-	var out int
-	for _, n := range nums {
-		out += n
-	}
-	return out
-}
-
-// func somar(a int) func(int) int {
-// 	return func(b int) int {
-// 		return a + b
-// 	}
-// }
-
-// chmando funções
-
-// func somar(a, b int) int {
-// 	return a + b
-// }
-
-// func swap(a, b int) (int, int) {
-// 	return b, a
-// }
-
-// func dividir(a, b int) (res int, rem int) {
-// 	res = a / b
-// 	rem = a & b
-// 	return res, rem
-// }
-
-//Nomes Publicos ou Privados
-
-//letra minúscula privado
-//letra Maiúscula pública
-
-//Nomes
-
-var Foo string
-
-const Bar string = "bar"
-
-type MeuTipo struct{}
