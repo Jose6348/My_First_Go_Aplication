8881d8b4fbfb8eff96f5978f932ce3e94d5457cf
Jose6348 <jfalasco88@gmail.com>
2025-02-13 01:14:25 -0300

    Loops e Atualizando a Linguagem

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	const x = 10
-	arr := [x]int{5: 400, 7: 300}
-	fmt.Println(arr)
+	for range 10 {
+		fmt.Println("dentro")
+	}
+
+	// arr := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	// for _, elem := range arr {
+	// 	fmt.Println(elem)
+	// }
 }
