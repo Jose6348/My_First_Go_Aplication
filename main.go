0622ae33919f8841218c261c936828a6421c1b55
Jose6348 <jfalasco88@gmail.com>
2025-02-13 00:54:54 -0300

    Sistema de Tipos e Constantes

--- main.go
+++ main.go
@@ -4,12 +4,53 @@ import "fmt"
 
 // "myFirstGoProject/pacote"
 
-var idade = 30
+//bool:
+
+// Inteiros :
+
+// int int8 int16
+
+//uint uint8 uint16 uintptr
+
+// Type Alias:
+
+// byte = uint8
+
+//rune = int32
+
+//float:
+
+//float32 float64
+
+//complexos:
+
+//complex64 complex128
 
 func main() {
-	foo, bar := "foo", 50
-	nome := "Pedro"
-	sobrenome := "Pessoa"
-	fmt.Println(nome, sobrenome, idade)
-	fmt.Println(foo, bar)
+	const x = 10
+
+	TakeInt32(x)
+	TakeInt64(x)
+	TakeString("foo")
+	TakeFloat(3.14)
+	// x := 10084
+	// s := strconv.FormatInt(int64(x), 10)
+
+	// fmt.Println(s)
+}
+
+func TakeFloat(f float64) {
+	fmt.Println(f)
+}
+
+func TakeString(s string) {
+	fmt.Println(s)
+}
+
+func TakeInt32(x int32) {
+	fmt.Println(x)
+}
+
+func TakeInt64(x int64) {
+	fmt.Println(x)
 }
