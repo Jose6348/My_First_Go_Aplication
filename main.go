8304b69d8e408bb0df14490e96c397cc26666f3c
Jose6348 <jfalasco88@gmail.com>
2025-02-13 21:54:05 -0300

    Switches

--- main.go
+++ main.go
@@ -1,16 +1,44 @@
 package main
 
 import (
-	"fmt"
-	"math"
+	"time"
 )
 
 func main() {
-	if x := math.Sqrt(4); x < 1 {
-		fmt.Println(x)
-	} else if x < 1 {
-		fmt.Println("Maior que zero")
-	} else {
-		fmt.Print("caiu aqui")
+	// switch x := math.Sqrt(4); x {
+	// case 2:
+	// 	fmt.Println("resultado é 2")
+	// default:
+	// 	fmt.Println("Deu erro")
+	// }
+
+}
+
+func isWeekend(x time.Time) bool {
+	switch x.Weekday() {
+	case time.Sunday, time.Saturday:
+		return true
+	default:
+		return false
 	}
 }
+
+// func isWeekend(x time.Time) bool {
+// 	switch {
+// 	case x.Weekday() > 0 && x.Weekday() < 6:
+// 		return false
+// 	default:
+// 		return true
+// 	}
+
+// switch {
+// case x == 1:
+// 	fmt.Println(1)
+// 	fallthrough
+// case "äbc" == "foo":
+// 	fmt.Println(2)
+// default:
+// 	fmt.Println("Outra coisa")
+// }
+
+// }
